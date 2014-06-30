aa82e8c3b66bec8480ee2d8685dd4858ec655f72
Dias Aurélien <aurelien.esiao@gmail.com>
2014-06-30 22:30:53 +0200

    cleaning code. url and titles of pages fixed.

--- library.js
+++ library.js
@@ -3,20 +3,9 @@
 
 	var TopicColor = {};
 
-	var SocketAdmin = module.parent.require('./socket.io/admin'),
-		Sockets = module.parent.require('./socket.io/index'),
-		Topics = module.parent.require('./topics.js');
-
-	TopicColor.init = function() {
-		SocketAdmin.topics = SocketAdmin.topics || {};
-		SocketAdmin.topics.renameTopic = function(socket, data, callback) {
-			if (!data.tid || !data.title) {
-				return callback(false);
-			}
-
-			Topics.setTopicField(data.tid, 'title', data.title, callback);
-		};
-	};
+	var 	Topics = module.parent.require('./topics.js'),
+		User = module.parent.require('./user.js'),
+		Groups = module.parent.require('./groups.js');
 
 	module.exports = TopicColor;
 }(module));
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,20 @@
 (function() {
 	"use strict";
 
+	jQuery('document').ready(function() {
+
+		$(window).on('action:ajaxify.end', function(ev, data) {
+			if (data.url.match(/^category/) || data.url.match(/^unread/) || data.url.match(/^recent/) || data.url.match(/^popular/)) {
+				colorifyTopics();
+			}
+		});
+
+		$(window).on('action:categories.loaded', colorifyTopics);
+		$(window).on('action:categories.new_topic.loaded', colorifyTopics);
+		$(window).on('action:topic.loaded', colorifyTopics);
+
+	});
+
 	function colorifyTopics() {
 		//Change topic title on topic list
 		$('.category-item .topic-title').each(function() {
@@ -8,6 +22,8 @@
 			var reg = /#([0-9A-Fa-f]{6})([^#]*)/g;
 			if (title.html().match(reg)) {
 				title.html( title.html().replace(reg,'<font color="#$1">$2</font>') );
+				reg = /([0-9A-Fa-f]{6})/g;
+				title.attr('href', title.attr('href').replace(reg,'') );
 			}
 		});
 		//Change topic title on topic
@@ -26,34 +42,7 @@
 				title.html( title.html().replace(reg,'') );
 			}
 		});
+		var reg = /#([0-9A-Fa-f]{6})/g;
+		document.title = document.title.replace(reg, '');
 	}
-
-	jQuery('document').ready(function() {
-		$(window).on('action:ajaxify.end', function(ev, data) {
-			if (data.url.match(/^category/) || data.url.match(/^unread/) || data.url.match(/^recent/) || data.url.match(/^popular/)) {
-				colorifyTopics();
-			}
-
-			if (data.url.match(/^topic/)) {
-
-				/* Does not work yet */
-
-				//Change topic <title>
-				var reg = /#([0-9A-Fa-f]{6})([^#]*)/g;
-
-				if (document.title.match(reg)) {
-					document.title = document.title.replace(/#[0-9A-Fa-f]{6}/g,'');
-				}
-				//Window Url don't have #
-				reg = /([0-9A-Fa-f]{6})/g;
-				if (window.location.href.match(reg)) {
-					document.title = window.location.href.replace(/#[0-9A-Fa-f]{6}/g,'');
-				}
-			}
-		});
-
-		$(window).on('action:categories.loaded', colorifyTopics);
-		$(window).on('action:categories.new_topic.loaded', colorifyTopics);
-		$(window).on('action:topic.loaded', colorifyTopics);
-	});
 }());
