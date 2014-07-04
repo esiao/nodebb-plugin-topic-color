c4449b039b33178ae120e564ed4ef8b1f654de52
Dias Aurélien <aurelien.esiao@gmail.com>
2014-07-04 22:54:14 +0200

    Code cleaned, plugin working

--- library.js
+++ library.js
@@ -1,11 +1,3 @@
 (function(module) {
 	"use strict";
-
-	var TopicColor = {};
-
-	var 	Topics = module.parent.require('./topics.js'),
-		User = module.parent.require('./user.js'),
-		Groups = module.parent.require('./groups.js');
-
-	module.exports = TopicColor;
 }(module));
